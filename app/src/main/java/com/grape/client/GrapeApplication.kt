da138af2b11d4cd797d7c595314ed3ad65d7a9ab
Coolguyhehe2515 <mobilemc022@gmail.com>
2026-09-26 21:24:23 +0700

    Initialize Grape logging before activities

--- app/src/main/java/com/grape/client/GrapeApplication.kt
+++ app/src/main/java/com/grape/client/GrapeApplication.kt
@@ -7,8 +7,13 @@ import com.grape.client.logging.GrapeLogger
 class GrapeApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
         GrapeLogger.initialize(this)
-        GrapeLogger.info("Grape Client startup")
+        GrapeLogger.info("Grape Client application started")
+        GrapeLogger.info("Package: $packageName")
+        GrapeLogger.info("Android SDK: ${android.os.Build.VERSION.SDK_INT}")
+        GrapeLogger.info("ABI: ${android.os.Build.SUPPORTED_ABIS.joinToString()}")
+
         GrapeCrashHandler.install(this)
     }
 }
