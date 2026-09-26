397320864e1769c316f306433539165db3c5eff5
Coolguyhehe2515 <mobilemc022@gmail.com>
2026-09-26 11:12:50 +0700

    Implement ARM64 native surface host

---
+++ app/src/main/cpp/grape_host.cpp
@@ -0,0 +1,38 @@
+#include "grape_host.h"
+
+#include <android/log.h>
+#include <mutex>
+
+namespace {
+std::mutex g_mutex;
+ANativeWindow* g_window = nullptr;
+}
+
+extern "C" void grape_host_set_surface(ANativeWindow* window) {
+    std::lock_guard<std::mutex> lock(g_mutex);
+
+    if (g_window == window) {
+        return;
+    }
+
+    if (g_window != nullptr) {
+        ANativeWindow_release(g_window);
+        g_window = nullptr;
+    }
+
+    if (window != nullptr) {
+        g_window = ANativeWindow_acquire(window);
+        __android_log_print(ANDROID_LOG_INFO, "GrapeHost", "Native surface attached");
+    }
+}
+
+extern "C" void grape_host_clear_surface() {
+    std::lock_guard<std::mutex> lock(g_mutex);
+
+    if (g_window != nullptr) {
+        ANativeWindow_release(g_window);
+        g_window = nullptr;
+    }
+
+    __android_log_print(ANDROID_LOG_INFO, "GrapeHost", "Native surface detached");
+}
